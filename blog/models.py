0fb9cea6b9f7e83da2a0f38b8e3d49d96c7add59
Boborahim <boborahimrustamqulov0@gmail.com>
2024-12-09 19:12:08 +0500

    Update

--- blog/models.py
+++ blog/models.py
@@ -4,12 +4,13 @@ from django.contrib.auth.models import User
 class Article(models.Model):
     author = models.ForeignKey(User,on_delete=models.CASCADE)
 
+    # id = models.IntegerField(primary_key=True)
     title = models.CharField(max_length=200)
     description = models.TextField()
     is_active = models.BooleanField(default=False)
     image = models.ImageField(upload_to="Article/image")
     create_data = models.DateTimeField(auto_now=True)
-    blog_link = models.URLField(blank=True, null=True)
+    #4ta maydon qo'shish
     def __str__(self) -> str:
         return f"{self.title}"
 
@@ -46,9 +47,12 @@ class Portfolio(models.Model):
     def __str__(self) -> str:
         return f"{self.title}"
 
+
+
 class Comment(models.Model):
     first_name = models.CharField(max_length=50)
     text = models.TextField()
     create_date = models.DateTimeField(auto_now=True)
     rating = models.IntegerField()
+
     article = models.ForeignKey(Article,on_delete=models.CASCADE)
