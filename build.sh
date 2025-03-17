075d284a3358ed6e1d5a2fc7f00fd68a766ad8d0
BoborahimAlisherovich <boborahimrustamqulov0@gmail.com>
2025-03-17 18:07:20 +0500

    update

--- build.sh
+++ build.sh
@@ -6,4 +6,6 @@ pip install -r requirements.txt
 # Convert static asset files
 python manage.py collectstatic --no-input
 # Apply any outstanding database migrations
+python manage.py makemigrations
+
 python manage.py migrate
